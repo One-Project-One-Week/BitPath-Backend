230b543a08a4060c0175e87b44eee16a904371d0
aungkyawzaww <aungkyawzaww7889@gmail.com>
2025-04-23 14:46:36 +0630

    resource and link

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,15 @@
 
 use App\Http\Controllers\Api\JwtAuthController;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Api\RecommandResourcesController;
+use App\Http\Controllers\Api\ResourcelinksController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
 
 
 Route::group(['prefix' => 'auth'], function(){
@@ -16,4 +20,13 @@ Route::group(['prefix' => 'auth'], function(){
     Route::get('profile', [JwtAuthController::class, 'profile'])->middleware('jwtauthmiddleware');
     Route::post('logout', [JwtAuthController::class, 'logout'])->middleware('jwtauthmiddleware');
     Route::post('refresh', [JwtAuthController::class, 'refresh'])->middleware('jwtauthmiddleware');
+
+    
+});
+
+Route::group(['middleware' => ['jwtauthmiddleware']], function(){
+    
+    Route::resource('/recommandresources',RecommandResourcesController::class);
+    Route::resource('/resoucelinks',ResourcelinksController::class);
+
 });
