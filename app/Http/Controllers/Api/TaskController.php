5cf27c505c774c464167f7f65c2bc595e2e849ac
MyoLatt <lattzinmyo3@gmail.com>
2025-04-25 21:39:11 +0630

    task error fixing

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -17,9 +17,6 @@ class TaskController extends Controller
         // getting a plan of a task
         $plan = $task->plan;
 
-        $user->update([
-            'last_studied_date' => now(),
-        ]);
 
         // updating a task as completed
         if($task->is_finished == false){
@@ -32,6 +29,14 @@ class TaskController extends Controller
                 'message' => 'Task already completed',
             ], 400);
         }
+
+        
+        if($user->last_studied_date == null){
+            $user->update([
+                'last_studied_date' => now(),
+            ]);
+        }
+
 
         // adding a plan completed task by one
         $plan->update([
@@ -48,14 +53,19 @@ class TaskController extends Controller
         $current_date = now();
         $day_diff = $last_studied_date->diffInDays($current_date);
 
-        if($day_diff < 1){
+        if($day_diff > 1 && $day_diff < 2){
             $user->update([
                 'current_streak' => $user->current_streak + 1,
                 'last_studied_date' => now(),
             ]);
-        }else{
+        }else if($day_diff > 2){
             $user->update([
                 'current_streak' => 1,
+                'last_studied_date' => now(),
+            ]);
+        }
+        else{
+            $user->update([
                 'last_studied_date' => now(),
             ]);
         }
